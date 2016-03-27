6320ca811dea6f7a06a416771c6926b478fc402a
Zach Ballard <zbginji@gmail.com>
2016-03-26 21:42:26 -0500

    initial commit

---
+++ VotingAPI/Candidate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VotingAPI
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hometown { get; set; }
+        public string District { get; set; }
+        public string Party { get; set; }
+        public virtual List<Race> Races { get; set; } 
+    }
+}
---
+++ VotingAPI/Vote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VotingAPI
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public virtual Voter Voter { get; set; }
+        public virtual Race Race { get; set; }
+    }
+}
