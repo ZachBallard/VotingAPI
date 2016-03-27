aac176894ed85e1aed405ce085af393d73384174
Zach Ballard <zbginji@gmail.com>
2016-03-27 16:07:11 -0500

    preparing for new controller commands using key

--- VotingAPI/App_Start/RouteConfig.cs
+++ VotingAPI/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace VotingAPI
@@ -18,6 +14,11 @@ namespace VotingAPI
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "With Key",
+                url: "{controller}/{key}/{action}/{id}",
+                defaults: new {controller = "Home",/*KEY goes here?*/ action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
--- VotingAPI/Controllers/VotesController.cs
+++ VotingAPI/Controllers/VotesController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
-using VotingAPI;
 using VotingAPI.Models;
 
 namespace VotingAPI.Controllers
